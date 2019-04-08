12cf646f3a99b8ca4f1ed5bdbd05b2948632fb91
meres <szalkaititi@gmail.com>
2019-04-08 06:38:15 -0700

    implement alarm

--- train-sensor/src/main/java/hu/bme/mit/train/sensor/TrainSensorImpl.java
+++ train-sensor/src/main/java/hu/bme/mit/train/sensor/TrainSensorImpl.java
@@ -9,6 +9,7 @@ public class TrainSensorImpl implements TrainSensor {
 	private TrainController controller;
 	private TrainUser user;
 	private int speedLimit = 5;
+	private boolean alarm = false;
 
 	public TrainSensorImpl(TrainController controller, TrainUser user) {
 		this.controller = controller;
@@ -22,8 +23,31 @@ public class TrainSensorImpl implements TrainSensor {
 
 	@Override
 	public void overrideSpeedLimit(int speedLimit) {
+
+		if (speedLimit < 0 || speedLimit > 500) {
+			this.alarm = true;
+			return;
+		}else{
+			this.alarm = false;
+		}
+		double temp = this.speedLimit*0.5;
+		if(speedLimit > this.speedLimit+temp || speedLimit < this.speedLimit+temp){
+			this.alarm = true;
+			return;
+		}else{
+			this.alarm = false;
+		}
+
 		this.speedLimit = speedLimit;
 		controller.setSpeedLimit(speedLimit);
+	}
+
+	public boolean getAlarmState(){
+		return this.alarm;
+	}
+
+	public void setAlarmState(boolean alarm) {
+		this.alarm = alarm;
 	}
 
 }
