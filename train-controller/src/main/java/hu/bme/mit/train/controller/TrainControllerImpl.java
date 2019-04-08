ca69c7330aa39ff646ffc59af6d50b63d13692c4
Wicoka <wicoka@schdesign.hu>
2019-04-08 05:52:39 -0700

    implement timer

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -2,9 +2,13 @@ package hu.bme.mit.train.controller;
 
 import com.google.common.collect.HashBasedTable;
 import com.google.common.collect.Table;
+import com.sun.jmx.snmp.tasks.Task;
 import hu.bme.mit.train.interfaces.TrainController;
 import hu.bme.mit.train.interfaces.TrainController;
+
 import java.util.Date;
+import java.util.Timer;
+import java.util.*;
 
 
 public class TrainControllerImpl implements TrainController {
@@ -13,6 +17,12 @@ public class TrainControllerImpl implements TrainController {
 	private int referenceSpeed = 0;
 	private int speedLimit = 0;
 
+	Timer timer = new Timer();
+
+
+	public void run(){
+		timer.schedule(new TrainControllerImplTask(), 1000);
+	}
 	@Override
 	public void followSpeed() {
 		if (referenceSpeed < 0) {
@@ -56,4 +66,13 @@ public class TrainControllerImpl implements TrainController {
 		return this.speedLimit;
 	}
 
+}
+
+class TrainControllerImplTask extends TimerTask {
+	private TrainControllerImpl trainController;
+
+	// run is a abstract method that defines task performed at scheduled time.
+	public void run() {
+		trainController.followSpeed();
+	}
 }
